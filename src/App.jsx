7158dc270c99a757da0bab7e2f9009ad128d5ec8
Jorge Cruz <joshiecruz@gmail.com>
2021-09-27 19:52:51 -0500

    Crear card Razas de Perros

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import logo from './media/logo.png';
 import borderCollie from './media/borderCollie.jpg';
 import rhodesian from './media/rhodesian.jpg';
 
+
+
 function App() {
   return (
     <div className='App'>
@@ -10,7 +12,7 @@ function App() {
         <ul className='navbar'>
           <li>
             <img src={logo} alt='imagen' className='logo' />
-          </li>
+            </li>
           <li>
             <button className='botonGenerico mainButton'>Nuevo post</button>
           </li>
@@ -32,25 +34,29 @@ function App() {
         <section>
           <h1>Razas de Perros</h1>
           <ul className='breedCardContainer'>
-            <li className='breedCard'>
-              <div className='contenedorImagen'>
-                <img src={borderCollie} alt='Border Collie' />
-              </div>
-              <span className='breedTitle'>Border Collie </span>
-            </li>
-            <li className='breedCard'>
-              <div className='contenedorImagen'>
-                <img src={rhodesian} alt='Border Collie' />
-              </div>
-              <span className='breedTitle'>Rhodesian</span>
-            </li>
+            <CardRazasPerros nombreRaza="BorderCollie" imagen={borderCollie} color='backGroudVerde'/>
+            <CardRazasPerros nombreRaza="Rhodesian" imagen={rhodesian} color='backGroudRojo'/>
+            <CardRazasPerros nombreRaza="Rhodesian" imagen={rhodesian} color='backGroudVerde'/>
+            <CardRazasPerros nombreRaza="Rhodesian" imagen={rhodesian} color='backGroudRojo'/>
+            <CardRazasPerros nombreRaza="Rhodesian" imagen={rhodesian} color='backGroudVerde'/>
           </ul>
         </section>
         <section></section>
       </main>
-      <footer></footer>
+      <footer> </footer>
     </div>
   );
 }
-
+function CardRazasPerros ({nombreRaza, imagen, color}){
+  return(
+    <li className='breedCard'>
+      <div className={color}>
+          <div className='contenedorImagen'>
+          <img src={imagen} alt={nombreRaza} />
+           </div>
+           <span className='breedTitle'>{nombreRaza}</span>
+           </div>
+     </li>
+  );
+}
 export default App;
